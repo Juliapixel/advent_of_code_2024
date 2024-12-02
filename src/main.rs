ee13b13ae9c7e9aac55e8076537a4d7b9acdd1f8
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-12-01 21:15:17 -0300

    format nicelierererer

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,17 @@ macro_rules! day {
             if ARGS.part == 1 {
                 let start = ::std::time::Instant::now();
                 paste! {
-                    let _ = $result.insert(<crate::days::[< Day $day Part1 >] as ::advent_of_code_2024::Solution>::solve(input));
+                    let _ = $result.insert(
+                        <crate::days::[< Day $day Part1 >] as ::advent_of_code_2024::Solution>::solve(input)
+                    );
                 }
                 let _ = $time.insert(start.elapsed());
             } else if ARGS.part == 2 {
                 let start = ::std::time::Instant::now();
                 paste! {
-                    let _ = $result.insert(<crate::days::[< Day $day Part2 >] as ::advent_of_code_2024::Solution>::solve(input));
+                    let _ = $result.insert(
+                        <crate::days::[< Day $day Part2 >] as ::advent_of_code_2024::Solution>::solve(input)
+                    );
                 }
                 let _ = $time.insert(start.elapsed());
             }
@@ -30,7 +34,7 @@ macro_rules! day {
 }
 
 macro_rules! match_days {
-    ($result:ident, $time:ident, $($days:literal),+) => {
+    ($result:ident, $time:ident, [$($days:literal),+]) => {
         $(day!($result, $time, $days));+
     };
 }
@@ -38,10 +42,9 @@ macro_rules! match_days {
 fn main() {
     let mut result: Option<Box<dyn Display>> = None;
     let mut time: Option<Duration> = None;
-    match_days!(
-        result, time, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-        22, 23, 24, 25
-    );
+    match_days!(result, time, [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+    ]);
     match (result, time) {
         (Some(res), Some(time)) => {
             println!("result: {res}");
